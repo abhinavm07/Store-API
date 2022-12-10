3d3708f8765fc8466bc6136ddbcacdc7218127e6
abhinavm07 <88830300+abhinavm07@users.noreply.github.com>
2022-12-10 23:00:13 +0545

    new Port

--- controllers/logic.js
+++ controllers/logic.js
@@ -2,4 +2,9 @@ const homePage = (req, res) => {
   res.send("Welcome to the Home Page");
 };
 
-module.exports = { homePage };
+const products = async (req, res) => {
+  const storeData = null;
+  res.json({ data: storeData });
+};
+
+module.exports = { homePage, products };
--- routes/routeTasks.js
+++ routes/routeTasks.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const Router = express.Router();
-const { homePage } = require("../controllers/logic");
+const { homePage, products } = require("../controllers/logic");
 
 Router.get("/", homePage);
+Router.get("/api/v1/data", products);
 
 module.exports = Router;
